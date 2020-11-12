6ec4b1a1de0b91d07c9d4980867f6c4a4f023177
devHabit <szhiheng27@gmail.com>
2020-11-12 19:49:41 +0300

    Added FontAwesome5, FontAwesome5Pro and Fontisto icons.

--- src/getIconType.ts
+++ src/getIconType.ts
@@ -7,9 +7,12 @@ import FoundationIcon from 'react-native-vector-icons/Foundation';
 import EvilIcon from 'react-native-vector-icons/EvilIcons';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
+import FA5Icon from 'react-native-vector-icons/FontAwesome5';
+import FA5Brands from 'react-native-vector-icons/FontAwesome5Pro';
 import SimpleLineIcon from 'react-native-vector-icons/SimpleLineIcons';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import AntIcon from 'react-native-vector-icons/AntDesign';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
 
 const customIcons = {};
 
@@ -37,12 +40,18 @@ export default type => {
       return EntypoIcon;
     case 'font-awesome':
       return FAIcon;
+    case 'font-awesome5':
+      return FA5Icon;
+    case 'font-awesome-brands':
+      return FA5Brands;
     case 'simple-line-icon':
       return SimpleLineIcon;
     case 'feather':
       return FeatherIcon;
     case 'antdesign':
       return AntIcon;
+    case 'fontisto':
+      return FontistoIcon;
     default:
       if (Object.prototype.hasOwnProperty.call(customIcons, type)) {
         return customIcons[type];
